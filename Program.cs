4c4a6ce6d8d62ed5be9b853818ca070fd0fcc4dc
aawee0 <aawee0@gmail.com>
2016-09-03 14:45:18 +1200

    Second commit
    Function names and structure changed

--- Program.cs
+++ Program.cs
@@ -9,40 +9,38 @@ namespace PalindromeCheck
 {
     class Program
     {
+        static bool IsPalindrome(string s)
+        {
+            // checking the string for non-letter characters
+            //bool lettersOnly = Regex.IsMatch(s, @"^[a-zA-Z]+$");
+            //if (lettersOnly == false) Console.WriteLine("Input should be a word that contains latin letters only, please try again.");
+            
+            // removing all the whitespaces from the string
+            string palin = Regex.Replace(s.ToLower(), @"\s+", "");
+
+            int len = palin.Length;
+            bool isPalin = true;
+            
+            // comparing the letters on both sides from the middle
+            for (int i = 0; i < len / 2; i++) if (palin[i] != palin[len - 1 - i]) isPalin = false;
+
+            return isPalin;
+        }
+
+        static void Check(string s, bool shouldBePalindrome)
+        {
+            Console.WriteLine(IsPalindrome(s) == shouldBePalindrome ? "pass" : "FAIL");
+        }
+
         static void Main(string[] args)
         {
-            string palin = null; // palindrome string
-            bool noArgs = false; // a variable indicating lack of arguments
+            Check("abcba", true);
+            Check("abcde", false);
+            Check("Mr owl ate my metal worm", true);
+            Check("Never Odd Or Even", true);
+            Check("Never Even Or Odd", false);
 
-            // trying to get a string out of command line arguments
-            try
-            {
-                palin = args[0];
-            }
-            catch (Exception e)
-            {
-                noArgs = true;
-                Console.WriteLine("Incorrect argument. Usage: PalindromeCheck [word]");
-            }
-
-            // if an argument is in place, continue
-            if (noArgs == false)
-            {
-                // checking the string for non-letter characters
-                bool lettersOnly = Regex.IsMatch(palin, @"^[a-zA-Z]+$");
-                if (lettersOnly == false) Console.WriteLine("Input should be a word that contains latin letters only, please try again.");
-                else
-                {
-                    // if the input is correct, compare the letters
-                    int len = palin.Length;
-
-                    bool isPalin = true;
-                    for (int i = 0; i < len / 2; i++) if (palin[i] != palin[len - 1 - i]) isPalin = false;
-
-                    if (isPalin) Console.Write("\"" + palin + "\"" + " is a palindrome.");
-                    else Console.Write("\"" + palin + "\"" + " is NOT a palindrome.");
-                }
-            }
+            
         }
     }
 }
